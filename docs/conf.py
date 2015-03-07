1bd46c14e4343b5dc4e247f24267d1988072dd39
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2015-03-07 09:55:50 +0000

    conf.py version issue 4

--- docs/conf.py
+++ docs/conf.py
@@ -55,11 +55,10 @@ copyright = u'2015, Björn Johansson'
 #version = '0.1'
 
 # The full version uisng versioneer
-import versioneer
 #import imp
-#fp, pathname, description = imp.find_module('versioneer')
-#versioneer = imp.load_module('versioneer', fp, pathname, description)
-#del imp
+fp, pathname, description = imp.find_module('versioneer')
+versioneer = imp.load_module('versioneer', fp, pathname, description)
+del imp
 versioneer.VCS = 'git'
 versioneer.versionfile_source = '../pydna/_version.py'
 versioneer.versionfile_build = '../pydna/_version.py'
