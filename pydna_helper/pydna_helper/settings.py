f0e0df120c99df52488a33f3a17f36d266652dd9
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2015-06-12 18:46:19 +0100

    added git_exp

--- pydna_helper/pydna_helper/settings.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-apecommand               = u"tclsh /home/bjorn/.ApE/apeextractor/ApE.vfs/lib/app-AppMain/AppMain.tcl"
-page_with_primers        = u"/home/bjorn/Dropbox/wikidata/Primers.wiki"
-
-
