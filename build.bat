09125bfad0745237cf0e6e8af486e095fa157dae
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2015-06-29 11:11:48 +0100

    123

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
+
+conda config --add channels ulmo
+conda config --add channels synthicity
+
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash                 # This “shebang” tells what program to use to interpret the script.
-echo $PWD
+
+conda config --add channels ulmo
+conda config --add channels synthicity 
+
 $PYTHON setup.py install    # Python command to install the script.
