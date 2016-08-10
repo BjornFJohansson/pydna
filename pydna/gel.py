14f746cce39715ecefe9c8fec00a6242087bab86
Bruno Silva <bruno.phoenix@gmail.com>
2016-08-10 19:37:20 +0100

    Set `till_len` in `Gel.run()` to default to `np.inf` (instead of 1) when set to `None` (otherwise `till_len` would have to be set to more than one to allow bands to run out off the gel.

--- pydna/gel.py
+++ pydna/gel.py
@@ -1254,7 +1254,7 @@ class Gel:
             welly = np.repeat(welly, nlanes)
         welly = welly.to('cm')
         wellsep = wellsep.to('cm')
-        till_len = abs(till_len) if till_len is not None else 1
+        till_len = abs(till_len) if till_len is not None else np.inf
         if till_time is not None:
             till_time = to_units(till_time, 'hr', 'till_time').to('s')
         res = to_units(res, 'px/cm', 'res')
