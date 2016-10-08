f31cbedc2e23f9dc441d870424fbfcaf1b22fba9
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2016-10-08 15:52:30 +0100

    updated conda help script

--- build_and_upload_anaconda_pkgs_for_all_os.py
+++ build_and_upload_anaconda_pkgs_for_all_os.py
@@ -1,71 +1,78 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 print('''
 
-This script automatically builds, converts and uploads 
-conda packages.
+This script automatically builds, converts and uploads conda packages.
 
 It is meant to be placed in the same folder as the meta.yaml
-
-
 ''')
-
-
-
+from termcolor import colored
 import subprocess
-import os
+import pathlib
 
-cwd = os.getcwd()
-
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-os.chdir(script_dir)
-
-bashCommand = "conda build . --output "
+bashCommand = "conda build . --output"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
-pth = process.communicate()[0]
+
+pkgpth   = pathlib.Path(process.communicate()[0].strip())
+pkgname  = pkgpth.name
+builddir = pkgpth.parent.parent
+
 print(bashCommand)
-print(pth)
+print("Package  : ",colored( pkgpth, "blue"))
+print("Build dir: ",colored( builddir, "blue"))
+
 response = input("build (y/n) ? ")
 
 if response.lower().startswith("y"):
     bashCommand = "conda build . --no-anaconda-upload"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
     output = process.communicate()[0]
-    print(output)
+    print(colored(output, "blue"))
 else:
-    print("build skipped.")
+    print(colored("build skipped.", "red")) 
 
-response = input("convert (y/n) ? ")
-
-if response.lower().startswith("y"):
-    bashCommand = "conda convert {} -p all --output-dir conda-bld".format(pth)
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    output = process.communicate()[0]
-    print(output)
-else:
-    print("convert skipped.")
-    
-os.chdir("conda-bld")
-
-fn = os.path.basename(pth)
-
-for directory in os.listdir("."):
-    
-    bldpth = os.path.join(directory, fn)  
-    print(bldpth)
-    response = input("upload (y/n) ? ")
+if "/noarch/" in str(pkgpth): # no convert
+    print(colored("no convert, since package is NOARCH", "red"))
+    pkgdirs = ["noarch"]
+else: # ask to convert
+    response = input("convert (y/n) ? ")
     if response.lower().startswith("y"):
-        bashCommand = "anaconda upload {}".format(bldpth)
+        bashCommand = "conda convert {} -p all -o {}".format(str(pkgpth), str(builddir))
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
         output = process.communicate()[0]
-        print(output)
+        print(colored(output, "blue"))
+        pkgdirs = ["win-64", "osx-64", "win-32", "linux-64", "linux-32"]
     else:
-        print("upload skipped.")   
+        print(colored("convert skipped.", "red"))
+        pkgdirs = ["linux-64"]
 
-os.chdir(cwd)
 
+pts=[]
+
+print()
+print("The following packages have been created:")
+print()
+
+for dir_ in pkgdirs:
+    upload_pth = builddir.joinpath(dir_, pkgname)
+    print(colored(upload_pth, "blue"))
+    pts.append(upload_pth)
+    
+response = input("upload (y/n) ? ")
+
+if response.lower().startswith("y"):
+    for p in pts:
+        bashCommand = "anaconda upload {}".format(p)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
+        output = process.communicate()[0]
+        print(colored(output, "blue"))
+    else:
+        print("upload skipped.")
+    
+process = subprocess.Popen("conda build purge".split(), stdout=subprocess.PIPE, universal_newlines=True) 
+output = process.communicate()[0]
+print(output)
 print("done!")
