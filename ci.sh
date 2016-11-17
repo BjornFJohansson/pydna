ef487261114213f846f01a15ab0b42474aff573d
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2016-11-17 06:22:28 +0000

    fix branch in ci.sh

--- ci.sh
+++ ci.sh
@@ -1,4 +1,3 @@
-echo -e "Establish git variables:\n=============================================================="
 tagname="$(git describe --abbrev=0 --tags)"
 tag="$(git rev-list $tagname | head -n 1)"
 com="$(git rev-parse HEAD)"
@@ -6,11 +5,16 @@ dirty=$(git describe --tags --dirty --always)
 msg=$(git log -1 --pretty=%B)
 GITBRANCH="$(git rev-parse --abbrev-ref HEAD)"
 TRAVISBRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-branch=${DRONE_BRANCH:-$TRAVISBRANCH$APPVEYOR_REPO_BRANCH$GITBRANCH}
+branch=${DRONE_BRANCH:-$TRAVISBRANCH}
+branch=${branch:-$APPVEYOR_REPO_BRANCH}
+branch=${branch:-$GITBRANCH}
+echo "Establish git variables:"
+echo "=============================================================="
 echo "Branch              : $branch"
 echo "Current commit hash : $com"
 echo "Dirty tag           : $dirty"
 echo "Commit msg          : $msg"
+echo "Environment variables:"
 echo "=============================================================="
 echo "CI                   = $CI"
 echo "DRONE                = $DRONE"
