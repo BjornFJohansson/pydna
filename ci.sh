e5050131e3ab8a98a669107cfa8feec83921a0ca
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2016-11-17 06:33:25 +0000

    fix branches

--- ci.sh
+++ ci.sh
@@ -8,16 +8,55 @@ TRAVISBRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 branch=${DRONE_BRANCH:-$TRAVISBRANCH}
 branch=${branch:-$APPVEYOR_REPO_BRANCH}
 branch=${branch:-$GITBRANCH}
+echo "=============================================================="
 echo "Establish git variables:"
 echo "=============================================================="
 echo "Branch              : $branch"
 echo "Current commit hash : $com"
 echo "Dirty tag           : $dirty"
 echo "Commit msg          : $msg"
+echo "=============================================================="
 echo "Environment variables:"
 echo "=============================================================="
 echo "CI                   = $CI"
 echo "DRONE                = $DRONE"
 echo "TRAVIS               = $TRAVIS"
 echo "APPVEYOR             = $APPVEYOR"
+echo "=============================================================="
+echo "Build information:"
+echo "=============================================================="
+if [[ "$com" = "$tag" ]]
+then
+    echo "Tagged commit      : $tagname"
+    tagged_commit=true
+    re_final="^[0-9]\.[0-9]\.[0-9]$"
+    re_alpha="^[0-9]\.[0-9]\.[0-9]a[0-9]+$"
+    if [[ $tagname =~  $re_final ]]&&[[ $branch = "py3" ]]&&[[ $dirty = $tagname ]]
+    then
+        echo "Release tag and branch indicate Final release"
+        echo "deploy to pypi and anaconda.org with label 'main'."
+        echo "This is only done from the py3 branch"
+        pypiserver="pypi"
+        condalabel="main"
+    elif  [[ $tagname =~ $re_alpha ]]&&[[ $branch = "py3dev" ]]&&[[ $dirty = $tagname ]]
+    then
+        echo "Release tag and branch indicate Alpha release"
+        echo "deploy to testpypi and anaconda.org with label 'test'."
+        echo "This is only done from the py3dev branch"
+        pypiserver="testpypi"
+        condalabel="test"
+    else
+        echo "Build cancelled because"
+        echo "Release tag ($tagname) was not recognized"
+        echo "or"
+        echo "branch ($branch) was not py3 or py3dev"
+        echo "or"
+        echo "$dirty != $tagname"
+        exit 1
+    fi
+else
+    echo "Commit not tagged"
+    echo "Run test suite, No build or install"
+    tagged_commit=false
+fi
 echo "=============================================================="
