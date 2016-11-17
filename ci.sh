4149bcc2192a16edef8c5af1afd0274f285eb37d
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2016-11-17 06:16:30 +0000

    fix branch in ci.sh

--- ci.sh
+++ ci.sh
@@ -4,11 +4,10 @@ tag="$(git rev-list $tagname | head -n 1)"
 com="$(git rev-parse HEAD)"
 dirty=$(git describe --tags --dirty --always)
 msg=$(git log -1 --pretty=%B)
-
-branch=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-
+GITBRANCH="$(git rev-parse --abbrev-ref HEAD)"
+TRAVISBRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
+branch=${DRONE_BRANCH:-$TRAVISBRANCH$APPVEYOR_REPO_BRANCH$GITBRANCH}
 echo "Branch              : $branch"
-
 echo "Current commit hash : $com"
 echo "Dirty tag           : $dirty"
 echo "Commit msg          : $msg"
@@ -18,11 +17,3 @@ echo "DRONE                = $DRONE"
 echo "TRAVIS               = $TRAVIS"
 echo "APPVEYOR             = $APPVEYOR"
 echo "=============================================================="
-
-echo "APPVEYOR_REPO_BRANCH = $APPVEYOR_REPO_BRANCH"
-
-echo "$DRONE_COMMIT: the commit hash currently being built"
-echo "$DRONE_BRANCH: the branch currently being built"
-echo "$BUILD_ID: the current build number"
-echo "$GIT_COMMIT: the commit hash currently being built"
-echo "$GIT_BRANCH: the branch currently being built"
