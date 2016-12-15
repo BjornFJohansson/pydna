2092647599c9c9b74890f22f5aa852fef21f0fd3
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2016-12-15 05:31:03 +0000

    skip

--- ci.sh
+++ ci.sh
@@ -161,7 +161,7 @@ then
     elif [[ $APPVEYOR = true ]]||[[ $APPVEYOR = True ]]
     then
         echo "APPVEYOR: python setup.py bdist_wininst"
-        python setup.py bdist_exe
+        python setup.py bdist_wininst
         appveyor PushArtifact dist/*
         echo "APPVEYOR: exe package is registered"
         twine register -r $pypiserver dist/pydna*.exe
