98d52f23433a0970425d4af7cf1339ae3c48dfb7
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-01-13 06:35:41 +0000

    fix appveyor

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+echo "build.bat running"
 "%PYTHON%" setup.py build
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash                 # This “shebang” tells what program to use to interpret the script.
+echo "build.sh running"
 $PYTHON setup.py build
 $PYTHON setup.py install    # Python command to install the script.
