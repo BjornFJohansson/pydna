a1a7623c82b0fb82fa3f0beeea3af587c481613d
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-05-06 11:12:30 +0100

    try fix ci

--- ci.sh
+++ ci.sh
@@ -131,8 +131,8 @@ then
     conda-build -V
     conda create -yq -n pydnacondabuild35 python=3.5 anaconda-client pypandoc pandoc nbval
     conda create -yq -n pydnacondabuild36 python=3.6 anaconda-client pypandoc pandoc nbval
-    conda create -yq -n pydnapipbuild35   python=3.5 anaconda-client urllib3 twine pypandoc pandoc
-    conda create -yq -n pydnapipbuild36   python=3.6 anaconda-client twine pypandoc pandoc
+    conda create -yq -n pydnapipbuild35   python=3.5 anaconda-client urllib3  pypandoc pandoc
+    conda create -yq -n pydnapipbuild36   python=3.6 anaconda-client pypandoc pandoc
     conda create -yq -n twine             python=3.5 twine
     rm -rf dist
     rm -rf build
