4c6bbb8af22c7ad63681dc1b2c61c472a8acc938
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-05-27 14:35:18 +0100

    fixed test

--- tests/test_module_amplicon.py
+++ tests/test_module_amplicon.py
@@ -75,7 +75,7 @@ def test_amplicon_dbd():
     
     assert repr(prod) == 'Amplicon(65)'
 
-    fig =('\nPfu-Sso7d (rate 15s/kb)\n'
+    fig =( r'\nPfu-Sso7d (rate 15s/kb)\n'
             'Two-step|    30 cycles |      |65bp\n'
             '98.0°C  |98.0C         |      |Tm formula: Pydna tmbresluc\n'
             '_____ __|_____         |      |SaltC 50mM\n'
@@ -103,14 +103,14 @@ def test_amplicon_dbd_low_gc():
     
     assert repr(prod) == 'Amplicon(55)'
 
-    fig =('\nPfu-Sso7d (rate 15s/kb)\n'
-            'Two-step|    30 cycles |      |65bp\n'
-            '98.0°C  |98.0C         |      |Tm formula: Pydna tmbresluc\n'
-            '_____ __|_____         |      |SaltC 50mM\n'
-            '00min30s|10s  \        |      |Primer1C 1.0µM\n'
-            '        |      \ 72.0°C|72.0°C|Primer2C 1.0µM\n'
-            '        |       \______|______|GC 81%\n'
-            '        |       0min 0s|10min |4-12°C\n')
+    fig =(r'\nPfu-Sso7d (rate 15s/kb)\n'
+             'Two-step|    30 cycles |      |65bp\n'
+             '98.0°C  |98.0C         |      |Tm formula: Pydna tmbresluc\n'
+             '_____ __|_____         |      |SaltC 50mM\n'
+             '00min30s|10s  \        |      |Primer1C 1.0µM\n'
+             '        |      \ 72.0°C|72.0°C|Primer2C 1.0µM\n'
+             '        |       \______|______|GC 81%\n'
+             '        |       0min 0s|10min |4-12°C\n')
      
     assert str(prod.pfu_sso7d_program())
 
