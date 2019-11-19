d01aae5c05ff1196553d08945ea39346e617fe6c
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-11-19 06:42:10 +0000

    added seq31 & guess_alphabet functions

--- pydna/utils.py
+++ pydna/utils.py
@@ -292,6 +292,125 @@ def flatten(*args): # flatten
     return output[::-1]
 
 
+def seq31(seq):
+    """Turn a three letter code protein sequence into one with one letter code.
+
+    The single input argument 'seq' should be a protein sequence using single
+    letter codes, as a python string.
+
+    This function returns the amino acid sequence as a string using the one
+    letter amino acid codes. Output follows the IUPAC standard (including
+    ambiguous characters B for "Asx", J for "Xle" and X for "Xaa", and also U
+    for "Sel" and O for "Pyl") plus "Ter" for a terminator given as an asterisk.
+
+    Any unknown
+    character (including possible gap characters), is changed into 'Xaa'.
+
+    e.g.
+    >>> from Bio.SeqUtils import seq3
+    >>> seq3("MAIVMGRWKGAR*")
+    'MetAlaIleValMetGlyArgTrpLysGlyAlaArgTer'
+
+    This function was inspired by BioPerl's seq3.
+    """
+    threecode = {'Ala':'A', 'Asx':'B', 'Cys':'C', 'Asp':'D',
+                 'Glu':'E', 'Phe':'F', 'Gly':'G', 'His':'H',
+                 'Ile':'I', 'Lys':'K', 'Leu':'L', 'Met':'M',
+                 'Asn':'N', 'Pro':'P', 'Gln':'Q', 'Arg':'R',
+                 'Ser':'S', 'Thr':'T', 'Val':'V', 'Trp':'W',
+                 'Tyr':'Y', 'Glx':'Z', 'Xaa':'X', 'Ter':'*',
+                 'Sel':'U', 'Pyl':'O', 'Xle':'J'
+                 }
+
+    nr_of_codons = int(len(seq)/3)
+    sequence = [seq[i*3:i*3+3].title() for i in range(nr_of_codons)]
+    padding = " "*2
+    return padding.join([threecode.get(aa,'X') for aa in sequence])
+
+
+def guess_alphabet(sequence:str):
+    '''
+    This function guesses the alphabet of a string representing a 
+    biological sequence.
+
+    '''
+    
+    import string
+
+    from Bio.Alphabet       import SingleLetterAlphabet
+    from Bio.Alphabet       import NucleotideAlphabet
+    from Bio.Alphabet       import ProteinAlphabet
+    from Bio.Alphabet.IUPAC import extended_protein
+    from Bio.Alphabet.IUPAC import protein
+    from Bio.Alphabet.IUPAC import ambiguous_dna
+    from Bio.Alphabet.IUPAC import unambiguous_dna
+    from Bio.Alphabet.IUPAC import extended_dna 
+    from Bio.Alphabet.IUPAC import ambiguous_rna
+    from Bio.Alphabet.IUPAC import unambiguous_rna
+
+    if len(sequence)<1:
+        return SingleLetterAlphabet()
+
+    for c in sequence:
+        if c not in string.printable:
+            return SingleLetterAlphabet()
+
+    xp = set(extended_protein.letters)
+    pr = set(protein.letters)
+
+    ad = set(ambiguous_dna.letters)
+    ud = set(unambiguous_dna.letters)
+    ed = set(extended_dna.letters)
+
+    ar = set(ambiguous_rna.letters)
+    ur = set(unambiguous_rna.letters)
+
+    all = xp|pr|ad|ud|ed|ar|ur
+
+    sequence_chars = set(sequence.upper())
+
+    if sequence_chars - all - set(string.punctuation+string.whitespace):
+        return SingleLetterAlphabet()
+
+    nucleic_count = 0
+
+    for letter in "GATCUNgatcun":
+        nucleic_count += sequence.count(letter)
+
+    if float(nucleic_count) / float(len(sequence)) >= 0.9: # DNA or RNA
+        if 'T' in sequence_chars and 'U' in sequence_chars:
+            alphabet = NucleotideAlphabet()
+        elif not sequence_chars-ud:
+            alphabet = unambiguous_dna
+        elif not sequence_chars-ad :
+            alphabet = ambiguous_dna
+        elif not sequence_chars-ed:
+            alphabet = extended_dna
+        elif not sequence_chars-ur:
+            alphabet = unambiguous_rna
+        elif not sequence_chars-ar:
+            alphabet = ambiguous_rna
+        else:
+            alphabet = NucleotideAlphabet()
+    else:
+        threecode = ['ALA', 'ASX', 'CYS', 'ASP','GLU', 'PHE', 'GLY', 'HIS',
+                     'ILE', 'LYS', 'LEU', 'MET','ASN', 'PRO', 'GLN', 'ARG',
+                     'SER', 'THR', 'VAL', 'TRP','TYR', 'GLX', 'XAA', 'TER',
+                     'SEL', 'PYL', 'XLE']
+        tc=set(threecode)
+        three_letter_alphabet = set( [ sequence[i:i+3] for i in range(0,len(sequence),3)] )
+        if not three_letter_alphabet - tc:
+            alphabet = "three letter code"
+        elif sequence_chars - pr:
+            alphabet = protein
+        elif sequence_chars - xp:
+            alphabet = extended_protein
+        else:
+            alphabet = ProteinAlphabet()
+    return alphabet
+
+
+
 if __name__=="__main__":
     import os as _os
     cached = _os.getenv("pydna_cached_funcs", "")
