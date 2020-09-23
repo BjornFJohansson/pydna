663a290dd6c6fb0b1c7e5a766a509ebe22dd101e
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-09-23 16:29:51 +0100

    simplified run_test.py

--- run_test.py
+++ run_test.py
@@ -5,8 +5,6 @@ import os
 import logging
 import tempfile
 import pytest
-import pathlib
-import pkg_resources
 
 def main():
 
@@ -15,44 +13,24 @@ def main():
     os.environ["pydna_config_dir"] = tempfile.mkdtemp(prefix="pydna_config_dir_")
     os.environ["pydna_loglevel"] = str(logging.DEBUG)
 
-    installed = {pkg.key for pkg in pkg_resources.working_set}
+    args = [ "tests",
+             "src",
+             "--capture=no",
+             "--durations=10",
+             "--cov=pydna",
+             "--cov-report=html",
+             "--cov-report=xml",
+             "--import-mode=importlib",
+             "--nbval",
+             "--current-env",
+             "--doctest-modules",
+             "--capture=no",
+             "--import-mode=importlib",
+             "-vvv"]
 
-    args = []
+    result_suite = pytest.cmdline.main(args)
 
-    if "coverage" in installed:
-        print("coveralls-python is installed.")
-
-        args = ["--cov=pydna",
-                "--cov-report=html",
-                "--cov-report=xml",
-                "--import-mode=importlib"]
-    else:
-        print("coverage NOT installed! (pip install coverage)")
-
-    if "nbval" in installed:
-        print("nbval is installed.")
-        args.append("--nbval")
-        args.append("--current-env")
-    else:
-        print("nbval NOT installed! (pip install nbval)")
-
-    mainargs = ["tests", "--capture=no", "--durations=10"] + args
-
-    result_suite = pytest.cmdline.main(mainargs)
-
-    from pydna import __file__ as pydnainit
-
-    doctestdir = str(pathlib.Path(pydnainit).parent)
-
-    doctestargs = [
-        doctestdir,
-        "--doctest-modules",
-        "--capture=no",
-        "--import-mode=importlib",
-    ]
-    result_doctest = pytest.cmdline.main(doctestargs)
-
-    return result_doctest and result_suite
+    return result_suite
 
 
 if __name__ == "__main__":
