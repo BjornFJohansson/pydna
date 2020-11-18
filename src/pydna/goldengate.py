4fce68afa607c3d05a83524d3f27f0d49699803c
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-11-18 07:37:47 +0000

    goldengate

---
+++ src/pydna/goldengate.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2013-2020 by Björn Johansson.  All rights reserved.
+# This code is part of the Python-dna distribution and governed by its
+# license.  Please see the LICENSE.txt file that should have been included
+# as part of this package.
+
+#                                          ^^
+#     ^^      ..                                       ..
+#             []                                       []
+#           .:[]:_          ^^                       ,:[]:.
+#         .: :[]: :-.                             ,-: :[]: :.
+#       .: : :[]: : :`._                       ,.': : :[]: : :.
+#     .: : : :[]: : : : :-._               _,-: : : : :[]: : : :.
+# _..: : : : :[]: : : : : : :-._________.-: : : : : : :[]: : : : :-._
+# _:_:_:_:_:_:[]:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:[]:_:_:_:_:_:_
+# !!!!!!!!!!!![]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!![]!!!!!!!!!!!!!
+# ^^^^^^^^^^^^[]^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^[]^^^^^^^^^^^^^
+#             []                                       []
+#             []                                       []
+#             []                                       []
+#  ~~^-~^_~^~/  \~^-~^~_~^-~_^~-^~_^~~-^~_~^~-~_~-^~_^/  \~^-~_~^-~~-
+# ~ _~~- ~^-^~-^~~- ^~_^-^~~_ -~^_ -~_-~~^- _~~_~-^_ ~^-^~~-_^-~ ~^
+
+"""Assembly of sequences by GoldenGate ligation assebmly."""
+from pydna.dseqrecord import Dseqrecord as _Dseqrecord
+from copy import deepcopy as _deepcopy
+import logging as _logging
+
+_module_logger = _logging.getLogger("pydna." + __name__)
+
+from Bio.Restriction import BsaI, BsmBI, BbsI, FokI
+
+DNA = _Dseqrecord("gatcGAAGACtagagtctgattcg")
+
+a,b = DNA.cut(BbsI)
+
+assert a+b == DNA
+
+
+# MoClo
+
+
+
+
+
