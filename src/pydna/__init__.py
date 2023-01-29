1a029930365eb4e9639fd2be133762122c1adc14
BjornFJohansson <bjornjobb@gmail.com>
2023-01-29 08:44:54 +0000

    semver

--- src/pydna/__init__.py
+++ src/pydna/__init__.py
@@ -149,7 +149,7 @@ __license__ = "BSD"
 __maintainer__ = "Björn Johansson"
 __email__ = "bjorn_johansson@bio.uminho.pt"
 __status__ = "Development"  # "Production" #"Prototype"
-__version__ = "5.1.1a1"
+__version__ = "0.0.0"
 
 # create config directory
 _os.environ["pydna_config_dir"] = _os.getenv("pydna_config_dir",
