e29ac49f8e3a09c8dbc343b42adcbccd33c63473
BjornFJohansson <bjornjobb@gmail.com>
2023-05-24 14:26:35 +0100

    prep for adding new dundermethods

--- src/pydna/biosubclasses.py
+++ src/pydna/biosubclasses.py
@@ -49,7 +49,7 @@ class ExactPosition(_ExactPosition):
         result = cls.__new__(cls)
         memo[id(self)] = result
         for k, v in self.__dict__.items():
-            setattr(result, k, deepcopy(v, memo))
+            setattr(result, k, _deepcopy(v, memo))
         return result
 
 
