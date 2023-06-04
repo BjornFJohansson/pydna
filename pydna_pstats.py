a03c2a06a20afef95415f5f1786df2f71e921fd0
BjornFJohansson <bjornjobb@gmail.com>
2023-06-04 16:04:02 +0100

    only build on master

--- pydna_pstats.py
+++
@@ -1,9 +0,0 @@
-import pstats
-
-stats = pstats.Stats('prof/combined.prof')
-stats.sort_stats('cumulative')
-# stats.strip_dirs()
-stats.print_stats("pydna/src", 0.1)
-
-
-# stats.print_stats("local_path", 20) # Only show 20 of the listings
