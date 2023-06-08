c5fdbe3a1ceb560b345360c4c65fa9dbedb3b16e
BjornFJohansson <bjornjobb@gmail.com>
2023-06-08 09:02:47 +0100

    added some pre commit hooks

--- src/pydna/assembly.py
+++ src/pydna/assembly.py
@@ -52,6 +52,7 @@ from pydna.utils import rc as _rc
 from pydna._pretty import pretty_str as _pretty_str
 from pydna.contig import Contig as _Contig
 from pydna.common_sub_strings import common_sub_strings
+
 # from pydna.common_sub_strings import terminal_overlap
 from pydna.dseqrecord import Dseqrecord as _Dseqrecord
 import networkx as _nx
@@ -74,8 +75,6 @@ _module_logger = _logging.getLogger("pydna." + __name__)
 #     @_memorize("pydna.assembly.Assembly")
 #     def __call__(cls, *args, **kwargs):
 #         return super().__call__(*args, **kwargs)
-
-
 
 
 class Assembly(object):  # , metaclass=_Memoize):
