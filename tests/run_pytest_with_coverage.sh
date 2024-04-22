7bd04d24fe5a17b300cb02f9eb4abc1ec335f445
BjornFJohansson <bjornjobb@gmail.com>
2024-04-22 07:04:33 +0100

    cleaned gh-pages

--- tests/run_pytest_with_coverage.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-pytest . -v -s --cov=pydna --cov-report=html --cov-report=xml  #(in tests folder)
-# coverage run -m py.test && coverage html  #(in tests folder)
-read
--- tests/test_longerlocus_name_gives_dseq.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from pydna.readers import read
-
-
-def test_longername_gives_deseq():
-    myseq = read(open("./broken_genbank_files/pGreenLantern1.gb", "r").read())
-    assert myseq.circular
-    myseq = read(open("./broken_genbank_files/fakeGenBankFile.gb", "r").read())
-    assert myseq.circular
