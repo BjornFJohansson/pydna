165589e1ee5b33bfbc916a7aead0390701aedafc
Pei-Lun Xie <peilunx.jeff@gmail.com>
2024-08-19 23:44:52 +0100

    added docs+examples for Dseq_Features

--- docs/notebooks/run_code.py
+++ docs/notebooks/run_code.py
@@ -1,3 +1,4 @@
+#%%
 from pydna.parsers import parse
 from pydna.dseqrecord import Dseqrecord
 
@@ -5,11 +6,37 @@ file_path = '/Users/PeilunXie/Desktop/pydna/docs/notebooks/sequence.gb'
 files = parse(file_path)
 file = files[0]
 
-file.add_feature(2,4,type="ROI")
+file.add_feature(24,56, type_="gene")
 
 #List all the misc features
-misc_features = [f for f in file.features if f.type == "misc"]
+gene_features = [f for f in file.features if f.type == "gene"]
 #Select the misc feature of interest
-feature_of_interest = misc_features[0]
-#Annotate the feature using qualifiers
-feature_of_interest.qualifiers["note"] = ["This is the feature of interest"]
+print(gene_features)
+
+print(file.sorted_features())
+
+#%%
+from pydna.parsers import parse
+from pydna.dseqrecord import Dseqrecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation, CompoundLocation
+
+# Example sequence
+file_path = '/Users/PeilunXie/Desktop/pydna/docs/notebooks/sequence.gb'
+files = parse(file_path)
+file = files[0]
+
+# Define the locations of the exons (parts of the CDS)
+locations = [FeatureLocation(5, 15), FeatureLocation(20, 30)]
+
+# Create a compound location from these parts
+compound_location = CompoundLocation(locations)
+
+cds_feature = SeqFeature(location=compound_location, type="CDS", qualifiers={"gene": "example_gene"})
+
+# Add a feature with the compound location to the Dseqrecord
+file.feature.append(cds_feature)
+# Verify the added feature
+for feature in file.features:
+    print(feature)
+
+#%%
