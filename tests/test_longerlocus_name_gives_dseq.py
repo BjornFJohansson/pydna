610d3f6f66609c385548f5f560e23aecf830bf8a
BjornFJohansson <bjornjobb@gmail.com>
2024-10-03 12:22:37 +0100

    fix test sequence file

--- tests/test_longerlocus_name_gives_dseq.py
+++ tests/test_longerlocus_name_gives_dseq.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+
+import pytest
 from pydna.readers import read
 
 
 def test_longername_gives_deseq():
-    myseq = read(open("./broken_genbank_files/pGreenLantern1.gb", "r").read())
+    myseq = read(open("broken_genbank_files/pGreenLantern1.gb", "r").read())
     assert myseq.circular
-    myseq = read(open("./broken_genbank_files/fakeGenBankFile.gb", "r").read())
+    myseq = read(open("broken_genbank_files/fakeGenBankFile.gb", "r").read())
     assert myseq.circular
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-x", "-vvv", "-s"])
