2b010a960d1b9c7b4d95e2fcd7a1fc190b8499b1
Kawin <krayon.py@gmail.com>
2024-10-06 03:20:09 -0400

    typing for PrettyTable

--- src/pydna/_pretty.py
+++ src/pydna/_pretty.py
@@ -8,7 +8,9 @@ for for nicer string output in the IPython shell and Jupyter notebook.
 
 from prettytable import PrettyTable as _Pt
 from prettytable import MARKDOWN as _md
+
 from copy import copy as _copy
+from typing import List as _List
 
 
 class pretty_str(str):
@@ -21,15 +23,15 @@ class pretty_str(str):
 class PrettyTable(_Pt):
     """docstring."""
 
-    def lol(self):
+    def lol(self) -> _List[list]:
         """docstring."""
         return [self._field_names] + self._rows
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """docstring."""
         return self.get_string()
 
-    def _repr_markdown_(self):
+    def _repr_markdown_(self) -> pretty_str:
         c = _copy(self)
         c.set_style(_md)
         return pretty_str(c.get_string())
