fa0318258f9f216862d3e66ee47b87286dbc4827
Kawin <krayon.py@gmail.com>
2024-10-06 03:20:45 -0400

    typing for FakeSeq

--- src/pydna/fakeseq.py
+++ src/pydna/fakeseq.py
@@ -7,36 +7,41 @@
 """docstring."""
 
 
-class FakeSeq(object):
+class FakeSeq:
     """docstring."""
 
-    def __init__(self, length, n=50e-15, rf=0):  # 50 fmol = 0.05 pmol
-        self._length = int(length)
+    def __init__(
+        self,
+        length: int,
+        n: float = 50e-15,  # 50 fmol = 0.05 pmol
+        rf: float = 0.0,
+    ) -> None:
+        self._length = length
         self.n = n
         self.rf = rf
 
-    def m(self):
+    def m(self) -> float:
         """Mass of the DNA molecule in grams."""
         # M(Da) * n (mol) = g
         return self.M() * self.n
 
-    def M(self):
+    def M(self) -> float:
         """M grams/mol."""
         return (308.9 * self._length + 79.0) * 2
 
-    def __len__(self):
+    def __len__(self) -> int:
         """docstring."""
         return self._length
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         """docstring."""
         return self._length < len(other)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """docstring."""
         return f"FakeSeq({self._length:.1e})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         """docstring."""
         return self.__repr__()
 
