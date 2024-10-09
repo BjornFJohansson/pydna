902a1d51c217d6c2421f5f2b44a034667025dd6d
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-10-09 17:12:14 +0100

    update action

--- docs/make_markdown_notebooks.sh
+++ docs/make_markdown_notebooks.sh
@@ -3,5 +3,5 @@
 # Loop through all notebooks in the markdown_notebooks directory
 for notebook in notebooks/*.ipynb; do
     # Convert each notebook to markdown
-    jupyter nbconvert $notebook --to markdown --output-dir markdown_notebooks
+    python -m nbconvert $notebook --to markdown --output-dir markdown_notebooks
 done
